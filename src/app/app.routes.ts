37bc90a962362b1927833c343828e241d705e3a2
ricardo-redes <ricardo.anjos@redes.unb.br>
2025-04-10 20:21:28 -0300

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CompanyFormComponent } from './components/company-form/company-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'register', component: CompanyFormComponent },
+  { path: 'edit/:id', component: CompanyFormComponent },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonIcon } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
