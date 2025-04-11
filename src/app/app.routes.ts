eacb8285853b57a1f2b4a0d728d415b27e218a0c
ricardo-redes <ricardo.anjos@redes.unb.br>
2025-04-11 13:59:21 -0300

    lista de empresas e correção de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CompanyFormComponent } from './components/company-form/company-form.component';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'register', component: CompanyFormComponent },
-  { path: 'edit/:id', component: CompanyFormComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: '', component: CompaniesListComponent },
+      { path: 'register', component: CompanyFormComponent },
+      { path: 'edit/:id', component: CompanyFormComponent },
+    ],
+  },
   { path: '**', redirectTo: '' },
 ];
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../../service/theme.service';
 import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  public title = 'Empresas';
+  public subtitle = 'As emprasas cadastradas na Plataforma';
+
   public isDarkMode;
   public isSidebarOpen: boolean = false;
   public showSidebarContent: boolean = false;
 
-  constructor(private themeService: ThemeService) {
+  constructor(private themeService: ThemeService, private router: Router) {
     this.isDarkMode = this.themeService.darkMode$;
   }
 
   ngOnInit(): void {
     this.themeService.init();
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.router.url.includes('/register')) {
+          this.title = 'Adicionar Empresa';
+          this.subtitle = 'Preencher dados para adicionar empresa';
+        } else if (this.router.url.includes('/edit')) {
+          this.title = 'Editar Empresa';
+          this.subtitle = 'Alterar dados da empresa';
+        } else {
+          this.title = 'Empresas';
+          this.subtitle = 'As empresas cadastradas na Plataforma';
+        }
+      }
+    });
   }
 
   toggleDarkMode(): void {
